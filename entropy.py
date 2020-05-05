5070b77f27e1795699357911871c81e601d0ae3b
Nathan Gawith <NathanGawith@gmail.com>
2020-05-05 12:21:33 -0500

    parallelized python code

--- entropy.py
+++ entropy.py
@@ -1,16 +1,50 @@
-import math;
+# https://stackoverflow.com/questions/10117073/how-to-use-initializer-to-set-up-my-multiprocess-pool
+# https://stackoverflow.com/questions/10117073/how-to-use-initializer-to-set-up-my-multiprocess-pool
+from multiprocessing import Pool;
+import statistics;
 import functools;
+import math;
+import time;
 
-def getTerms(occurrences):
-    total = functools.reduce(lambda a, b: a + b, occurrences);
-    return list(map(lambda x: termFunction(x, x / total), occurrences));
+def init_worker(funcs, file, charsplit):
+    for func in funcs:
+        func.file = file;
+        func.charsplit = charsplit;
 
-# https://lerner.co.il/2014/05/11/creating-python-dictionaries-reduce/
-getOccurrences = lambda splitfile: list(functools.reduce(lambda d, current: d.update({current: d[current] + 1 if current in d.keys() else 1}) or d, splitfile, {}).values());
-getSplitFile = lambda file, charsplit: list(map(lambda x: file[(x[0] * charsplit):(x[0] * charsplit) + charsplit], enumerate([None] * math.ceil(len(file) / charsplit))));
-getEntropy = lambda splitfile: functools.reduce(lambda a, b: a + b, getTerms(getOccurrences(splitfile)));
-termFunction = lambda n, p: n * (-1 * p) * math.log(p, 2);
+def mapSplitFile(i):
+    return mapSplitFile.file[(i * mapSplitFile.charsplit):(i * mapSplitFile.charsplit) + mapSplitFile.charsplit];
 
-filename = 'WarAndPeace.txt'; # Shorter.txt
-with open(filename, encoding='UTF-8') as reader:
-    print(getEntropy(getSplitFile(reader.read(), 3)));
+def mapTerms(arg):
+    termFunction = lambda n, p: n * (-1 * p) * math.log(p, 2);
+    x = arg[0];
+    total = arg[1];
+    return termFunction(x, x / total);
+
+def main(file, charsplit, threads):
+    with Pool(processes=threads,initializer=init_worker, initargs=([mapSplitFile,mapTerms],file,charsplit,)) as pool:
+        start = time.time();
+        splitfile = pool.map(mapSplitFile, range(math.ceil(len(file) / charsplit)));
+        occurrences = list(functools.reduce(lambda d, current: d.update({current: d[current] + 1 if current in d.keys() else 1}) or d, splitfile, {}).values());
+        total = functools.reduce(lambda a, b: a + b, occurrences);
+        occurrences = [[x, total] for x in occurrences];
+        terms = pool.map(mapTerms, occurrences);
+        entropy = functools.reduce(lambda a, b: a + b, terms);
+        end = time.time();
+        return end - start, entropy;
+
+if __name__ == '__main__':
+    file = '';
+    with open('WarAndPeace.txt', encoding='UTF-8') as reader: # Shorter
+        file = reader.read();
+    output = '';
+    for charsplit in [1, 2, 3]:
+        for threads in [1, 2, 4, 8, 16, 32, 64]:
+            times = [];
+            for i in range(3):
+                result = main(file, charsplit, threads);
+                times.append(result[0]);
+                output += f'Total time with {threads} threads: {result[0]} seconds . . . entropy: {result[1]}';
+            output += f'\nAverage time with {threads} threads and {charsplit} charsplit: {statistics.mean(times)}\n';
+            print(f'\nAverage time with {threads} threads and {charsplit} charsplit: {statistics.mean(times)}\n');
+    with open('output.txt', 'w+') as writer:
+        writer.write(output);
